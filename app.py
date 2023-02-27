2954457a813e45fdac46e736410576e0a0a645c2
Jakub Zakrzewski <82118539+zakrzaq@users.noreply.github.com>
2023-02-27 13:33:58 +0100

    data stats
    Co-authored-by: Jakub Zakrzewski <jz.zakrzewsky@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from modules.greater import greet
-from services.test_data import get_data
+from services.test_data import get_data, get_data_stats
 
 greet()
 
@@ -7,3 +7,4 @@ data = get_data()
 
 if not data.empty:
     print(data.head())
+    print(get_data_stats())
